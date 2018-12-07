ffaf6cf3d2d52aae8081e291c92de7beca07ee43
Nerea Aparicio <nerea.aparicio@acuntia.dom>
2018-12-07 19:07:42 +0100

    add img

--- assets/js/models/game.js
+++ assets/js/models/game.js
@@ -2,7 +2,7 @@
 function Game() {
   this.secret = [];
   this.board = new Board();
-  this.selectables = [];
+  this.selectables = []; 
   this.colorsList = ["red", "blue", "yellow", "purple", "green", "orange"];
   //this.guess = 0;
   this.a = 0;
@@ -14,14 +14,18 @@ function Game() {
 }
 
 Game.prototype.init = function() {
+  this.topSecret(); //Tapa el secreto
   this.createSecret();
   this.createSelectables();
   this.renderSecret();
   this.renderBoard();
   this.renderSelectables(); 
 
-  this.checkCoincidence();
+}
 
+Game.prototype.topSecret = function (){
+  var hideSecret = document.getElementById("secret")
+  hideSecret.setAttribute("class","hide"); 
 }
 
 Game.prototype.createSecret = function() { //¡¡¡¡¡FUNCIONA!!!!!
@@ -52,7 +56,7 @@ Game.prototype.createSelectables = function() {
 
 // Prototipo para crear elementos en el DOM referente a this.selectables
 Game.prototype.renderSelectables = function (){
-  this.selectables.map(function(Token, index){
+  this.selectables.map(function(Token){
     var newDiv = document.createElement("div");
     newDiv.setAttribute("id", Token.color);
     newDiv.setAttribute("class", "token tokenSelectable");
@@ -60,9 +64,10 @@ Game.prototype.renderSelectables = function (){
     newDiv.style.backgroundColor = Token.color;
     newDiv.addEventListener("click", function (event){
       this.board.nextTile(Token)
-      this.changeColor (this.a, Token.color); //HACE FALTA LLAMAR A LA FUNCION QUE  PINTE
+      this.changeColor (this.a, Token.color); //HACE FALTA LLAMAR A LA FUNCION QUE  PINTE, ¡¡¡FUNCIONA!!!
       this.a++;
-      console.info('BOARD => ', this.board.table)
+        console.info('BOARD => ', this.board.table)
+      this.checkCoincidence(Token.color);
     }.bind(this))
     this.optionsDOM.appendChild(newDiv)
 
@@ -87,26 +92,28 @@ Game.prototype.renderBoard = function() {
   }.bind(this))
 }
 
-//Cambiar el color de la ficha por el color seleccionado
+//Cambiar el color de la ficha por el color seleccionado ¡¡¡FUNCIONA!!!
 Game.prototype.changeColor = function (num, color){
   var newColor = document.getElementsByClassName("token table")
-  newColor[num].style.backgroundColor = color;
-  
+  newColor[num].style.backgroundColor = color;  
 }
 
 
 
-Game.prototype.checkCoincidence = function() { //TIENE los SELECTABLES
+Game.prototype.checkCoincidence = function(color) { 
   var boardEnd = this.board.table.indexOf('')
   if(boardEnd % 4 === 0) {
     for(var i = boardEnd - 1; i > boardEnd - 5; i--) {
+      if(this.board.table[i].Token.color === this.secret[i]){
+       console.log ("si");
+    } else {
+      console.log ("no");
+    }
 
-      console.log ("que hay -> ", this.selectables)
-      // Revisar cómo hacemos el for
-      // this.checkWin(this.board.table[i])
+    // this.checkWin(this.board.table[i])
     }
   } else {
-    console.info('NO CONTAMOS')
+    console.info('NO CONTAMOS', this.board.table[1], "secreto", this.secret[boardEnd])
   }
 }
 
