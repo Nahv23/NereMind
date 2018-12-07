594748a7274d2d35b64c8dbfdcece0b2e5f9d578
Nerea Aparicio <nerea.aparicio@acuntia.dom>
2018-12-07 17:46:59 +0100

    draw colors on the board

--- assets/js/models/game.js
+++ assets/js/models/game.js
@@ -1,23 +1,27 @@
 
 function Game() {
-  this.secret = []
-  this.board = new Board()
-  this.selectables = []
-  this.colorsList = ["red", "blue", "yellow", "pink", "green", "orange"];
+  this.secret = [];
+  this.board = new Board();
+  this.selectables = [];
+  this.colorsList = ["red", "blue", "yellow", "purple", "green", "orange"];
   //this.guess = 0;
+  this.a = 0;
+
   // Inicialización de elementos del DOM
-  this.secretDOM = document.getElementById('secret')
-  this.boardDOM = document.getElementById('board')
-  this.optionsDOM = document.getElementById('options')
+  this.secretDOM = document.getElementById('secret');
+  this.boardDOM = document.getElementById('board');
+  this.optionsDOM = document.getElementById('options');
 }
 
 Game.prototype.init = function() {
-  this.createSecret()
-  this.createSelectables()
-  this.checkCoincidence()
+  this.createSecret();
+  this.createSelectables();
   this.renderSecret();
   this.renderBoard();
   this.renderSelectables(); 
+
+  this.checkCoincidence();
+
 }
 
 Game.prototype.createSecret = function() { //¡¡¡¡¡FUNCIONA!!!!!
@@ -27,7 +31,6 @@ Game.prototype.createSecret = function() { //¡¡¡¡¡FUNCIONA!!!!!
 }
 
 // Prototipo para crear elementos en el DOM referente a this.secret
-
 Game.prototype.renderSecret = function (){
   this.secret.map(function(color, index){
     var newDiv = document.createElement("div");
@@ -38,58 +41,8 @@ Game.prototype.renderSecret = function (){
     this.secretDOM.appendChild(newDiv)
   }.bind(this))
 
-
-  // this.tokensColors.appendChild (newDiv)
-  // if (index === arr.length -1) {
-  //   var button = document.createElement ("button");
-  //   button.innerText = "OK"
-  //   this.tokensColors.appendChild(button)
-  //   }
-  // })
 }
 
-// ================
-
-// Prototipo para crear elementos en el DOM referente a this.selectables
-Game.prototype.renderSelectables = function (){
-  this.selectables.map(function(Token){
-    var newDiv = document.createElement("div");
-    newDiv.setAttribute("id", Token.color);
-    newDiv.setAttribute("class", "token tokenSelectable");
-    newDiv.setAttribute("tabIndex", 1)
-    newDiv.style.backgroundColor = Token.color;
-    newDiv.addEventListener("click", function (event){
-      this.board.nextTile(Token)
-      console.info('BOARD => ', this.board.table)
-    }.bind(this))
-    this.optionsDOM.appendChild(newDiv)
-}.bind(this))
-
-}
-
-// ================
-
-// Prototipo para crear elementos en el DOM referente a this.board.table
-
-Game.prototype.renderBoard = function() {
-  this.board.table.map(function(Token) {
-    var newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "token table");
-    newDiv.setAttribute("tabIndex", 1)
-    // if(board.table === ""){ ///NO DIBUJA
-    //   newDiv.style.backgroundColor = Token.color;
-    //   console.log(color)
-    // } else{
-      newDiv.style.backgroundColor = "grey";
-      console.log(Token.color)
-
-    
-    
-    this.boardDOM.appendChild(newDiv)
-  }.bind(this))
-}
-
-// ===============
 
 Game.prototype.createSelectables = function() {
   this.selectables = this.colorsList.map(function(color) {
@@ -97,10 +50,58 @@ Game.prototype.createSelectables = function() {
   })
 }
 
-Game.prototype.checkCoincidence = function() {
+// Prototipo para crear elementos en el DOM referente a this.selectables
+Game.prototype.renderSelectables = function (){
+  this.selectables.map(function(Token, index){
+    var newDiv = document.createElement("div");
+    newDiv.setAttribute("id", Token.color);
+    newDiv.setAttribute("class", "token tokenSelectable");
+    newDiv.setAttribute("tabIndex", 1)
+    newDiv.style.backgroundColor = Token.color;
+    newDiv.addEventListener("click", function (event){
+      this.board.nextTile(Token)
+      this.changeColor (this.a, Token.color); //HACE FALTA LLAMAR A LA FUNCION QUE  PINTE
+      this.a++;
+      console.info('BOARD => ', this.board.table)
+    }.bind(this))
+    this.optionsDOM.appendChild(newDiv)
+
+    
+    // if (index === this.selectables.length -1) {
+    //   var button = document.createElement ("button");
+    //   button.innerText = "OK"
+    //   this.newDiv.appendChild(button)
+    //  }
+    }.bind(this))
+}
+
+
+// Prototipo para crear elementos en el DOM referente a this.board.table
+Game.prototype.renderBoard = function() {
+  this.board.table.map(function() {
+    var newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "token table");
+    newDiv.setAttribute("tabIndex", 1)
+    newDiv.style.backgroundColor = "grey";
+    this.boardDOM.appendChild(newDiv)
+  }.bind(this))
+}
+
+//Cambiar el color de la ficha por el color seleccionado
+Game.prototype.changeColor = function (num, color){
+  var newColor = document.getElementsByClassName("token table")
+  newColor[num].style.backgroundColor = color;
+  
+}
+
+
+
+Game.prototype.checkCoincidence = function() { //TIENE los SELECTABLES
   var boardEnd = this.board.table.indexOf('')
   if(boardEnd % 4 === 0) {
     for(var i = boardEnd - 1; i > boardEnd - 5; i--) {
+
+      console.log ("que hay -> ", this.selectables)
       // Revisar cómo hacemos el for
       // this.checkWin(this.board.table[i])
     }
